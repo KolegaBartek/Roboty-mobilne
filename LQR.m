ea9f450fda561ec213da100ee9d3a9bfba5fe01e
Joanna <162314255+Joanna610@users.noreply.github.com>
2024-06-06 16:49:38 +0200

    Finalna symulacja PID i LQR

---
+++ LQR.m
@@ -0,0 +1,28 @@
+M = 0.91;   % mass of the chassis  
+m = 0.4;    % mass of the wheels and shaft
+b = 0.1;    % estimate of viscous friction coefficient (N-m-s)
+I = 0.05;   % moment of inertia of the pendulum
+g = 9.81;   %acceleration due to gravity (m/s^2)
+l = 0.17;   %length to pendulum center of mass
+
+p = I*(M+m)+M*m*l^2; %denominator for the A and B matrices
+
+A = [0      1              0           0;
+     0 -(I+m*l^2)*b/p  (m^2*g*l^2)/p   0;
+     0      0              0           1;
+     0 -(m*l*b)/p       m*g*l*(M+m)/p  0];
+
+B = [0; (I+m*l^2)/p; 0; m*l/p];
+
+C = [1 0 0 0; 0 0 1 0];
+
+D = [0; 0];
+
+Q = [1    0     0     0
+     0     10     0     0
+     0     0     10     0
+     0     0     0     1];
+ 
+R = 100;
+
+K = lqr(A,B,Q,R)
